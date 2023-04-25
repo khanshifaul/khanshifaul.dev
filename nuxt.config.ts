5da5b68b38a7b19b0315fe622a294f4cc9aee236
Shifaul Islam <37363422+khanshifaul@users.noreply.github.com>
2023-04-25 15:46:29 +0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     app: {
         rootTag: "div",
-        baseURL: "/",
+        baseURL: "/khanshifaul.dev/",
     },
     modules: [
         '@nuxtjs/tailwindcss',
