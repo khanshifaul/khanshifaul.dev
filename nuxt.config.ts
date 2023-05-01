9c698b2cb9bf1f3d62a226cb6ff384cf57664b39
Shifaul Islam <37363422+khanshifaul@users.noreply.github.com>
2023-05-01 18:48:44 +0600

    modified:   nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,11 +22,11 @@ export default defineNuxtConfig({
     nitro: {
         preset: "node-server",
         routeRules: {
-            '/blog/**': { swr: true },
+            // '/blog/**': { swr: true },
             // '/blog/**': { swr: 600 },
             // '/blog/**': { static: true },
             // '/blog/**': { cache: { /* cache options*/ } },
-            '/assets/**': { headers: { 'cache-control': 's-maxage=0' } },
+            // '/assets/**': { headers: { 'cache-control': 's-maxage=0' } },
             // '/api/v1/**': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
             '/admin/dashboard': { redirect: '/admin' },
             // '/proxy/example': { proxy: 'https://example.com' },
